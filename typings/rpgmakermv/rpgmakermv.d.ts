d1c1ad2057e66f007441274133317c28eef31183
BaroqueEngine <rsakane+github@gmail.com>
2016-10-04 19:17:46 +0900

    rpg_sprites.jsのクラス群を追加。

--- typings/rpgmakermv/rpgmakermv.d.ts
+++ typings/rpgmakermv/rpgmakermv.d.ts
@@ -3250,3 +3250,381 @@ declare class Scene_Gameover extends Scene_Base
     isTriggered(): boolean;
     gotoTitle(): void;
 }
+
+declare class Sprite_Base extends Sprite
+{
+    initialize(): void;
+    update(): void;
+    hide(): void;
+    show(): void;
+    updateVisibility(): void;
+    updateAnimationSprites(): void;
+    startAnimation(animation: any, mirror: any, delay: number): void; // TODO
+    isAnimationPlaying(): boolean;
+}
+
+declare class Sprite_Button extends Sprite
+{
+    initialize(): void;
+    update(): void;
+    updateFrame(): void;
+    setColdFrame(x: number, y: number, width: number, height: number): void;
+    setHotFrame(x: number, y: number, width: number, height: number): void;
+    setClickHandler(method: Function): void;
+    callClickHandler(): void;
+    processTouch(): void;
+    isActive(): boolean;
+    isButtonTouched(): boolean;
+    canvasToLocalX(x: number): number;
+    canvasToLocalY(y: number): number;
+}
+
+declare class Sprite_Character extends Sprite_Base
+{
+    initialize(): void; // TODO
+    initialize(character: any): void; // TODO
+    initMembers(): void;
+    setCharacter(character: any): void; // TODO
+    update(): void;
+    updateVisibility(): void;
+    isTile(): boolean;
+    tilesetBitmap(tileId: number): any; // TODO
+    updateBitmap(): void;
+    isImageChanged(): boolean;
+    setTileBitmap(): void;
+    setCharacterBitmap(): void;
+    updateFrame(): void;
+    updateTileFrame(): void;
+    updateCharacterFrame(): void;
+    characterBlockX(): number;
+    characterBlockY(): number;
+    characterPatternX(): number;
+    characterPatternY(): number;
+    patternWidth(): number;
+    patternHeight(): number;
+    updateHalfBodySprites(): void;
+    createHalfBodySprites(): void;
+    updatePosition(): void;
+    updateAnimation(): void;
+    updateOther(): void;
+    setupAnimation(): void;
+    setupBalloon(): void;
+    startBalloon(): void;
+    updateBalloon(): void;
+    endBalloon(): void;
+    isBalloonPlaying(): boolean;
+}
+
+declare class Sprite_Battler extends Sprite_Base
+{
+    initialize(): void;
+    initialize(battler: any): void; // TODO
+    initMembers(): void;
+    setBattler(battler: any): void; // TODO
+    setHome(x: number, y: number): void;
+    update(): void;
+    updateVisibility(): void;
+    updateMain(): void;
+    updateBitmap(): void;
+    updateFrame(): void;
+    updateMove(): void;
+    updatePosition(): void;
+    updateAnimation(): void;
+    updateDamagePopup(): void;
+    updateSelectionEffect(): void;
+    setupAnimation(): void;
+    setupDamagePopup(): void;
+    damageOffsetX(): number;
+    damageOffsetY(): number;
+    startMove(x: number, y: number, duration: number): void;
+    onMoveEnd(): void;
+    isEffecting(): boolean;
+    isMoving(): boolean;
+    inHomePosition(): boolean;
+}
+
+declare class Sprite_Actor extends Sprite_Battler
+{
+    static MOTIONS: { [key: string]: { index: number, loop: boolean } };
+
+    initialize(): void;
+    initialize(battler: any): void; // TODO
+    initMembers(): void;
+    createMainSprite(): void;
+    createShadowSprite(): void;
+    createWeaponSprite(): void;
+    createStateSprite(): void;
+    setBattler(battler: any): void; // TODO
+    moveToStartPosition(): void;
+    setActorHome(index: number): void;
+    update(): void;
+    updateShadow(): void;
+    updateMain(): void;
+    setupMotion(): void;
+    setupWeaponAnimation(): void;
+    startMotion(motionType: string): void;
+    updateTargetPosition(): void;
+    updateBitmap(): void;
+    updateFrame(): void;
+    updateMove(): void;
+    updateMotion(): void;
+    updateMotionCount(): void;
+    motionSpeed(): number;
+    refreshMotion(): void;
+    startEntryMotion(): void;
+    stepForward(): void;
+    stepBack(): void;
+    retreat(): void;
+    onMoveEnd(): void;
+    damageOffsetX(): number;
+    damageOffsetY(): number;
+}
+
+declare class Sprite_Enemy extends Sprite_Battler
+{
+    initialize(): void;
+    initialize(battler: any): void; // TODO
+    initMembers(): void;
+    createStateIconSprite(): void;
+    setBattler(battler: any): void; // TODO
+    update(): void;
+    updateBitmap(): void;
+    loadBitmap(name: string, hue: number): void;
+    updateFrame(): void;
+    updatePosition(): void;
+    updateStateSprite(): void;
+    initVisibility(): void;
+    setupEffect(): void;
+    startEffect(effectType: string): void;
+    startAppear(): void;
+    startDisappear(): void;
+    startWhiten(): void;
+    startBlink(): void;
+    startCollapse(): void;
+    startBossCollapse(): void;
+    startInstantCollapse(): void;
+    updateEffect(): void;
+    isEffecting(): boolean;
+    revertToNormal(): void;
+    updateWhiten(): void;
+    updateBlink(): void;
+    updateAppear(): void;
+    updateDisappear(): void;
+    updateCollapse(): void;
+    updateBossCollapse(): void;
+    updateInstantCollapse(): void;
+    damageOffsetX(): number;
+    damageOffsetY(): number;
+}
+
+declare class Sprite_Animation extends Sprite
+{
+    initialize(): void;
+    initMembers(): void;
+    setup(target: any, animation: any, mirror: any, delay: number): void; // TODO
+    remove(): void;
+    setupRate(): void;
+    setupDuration(): void;
+    update(): void;
+    updateFlash(): void;
+    updateScreenFlash(): void;
+    absoluteX(): number;
+    absoluteY(): number;
+    updateHiding(): void;
+    isPlaying(): boolean;
+    loadBitmaps(): void;
+    isReady(): boolean;
+    createSprites(): void;
+    createCellSprites(): void;
+    createScreenFlashSprite(): void;
+    updateMain(): void;
+    updatePosition(): void;
+    updateFrame(): void;
+    currentFrameIndex(): void;
+    updateAllCellSprites(frame: any): void; // TODO
+    updateCellSprite(sprite: any, cell: any): void; // TODO
+    processTimingData(timing: any): void;
+    startFlash(color: any, duration: number): void; // TODO
+    startScreenFlash(color: any, duration: number): void; // TODO
+    startHiding(duration: number): void; // TODO
+}
+
+declare class Sprite_Damage extends Sprite
+{
+    initialize(): void;
+    setup(target: any): void;
+    digitWidth(): number;
+    digitHeight(): number;
+    createMiss(): void;
+    createDigits(baseRow: number, value: number): void;
+    createChildSprite(): Sprite;
+    update(): void;
+    updateChild(sprite: Sprite): void;
+    updateFlash(): void;
+    updateOpacity(): void;
+    isPlaying(): boolean;
+}
+
+declare class Sprite_StateIcon extends Sprite
+{
+    initialize(): void;
+    initMembers(): void;
+    setup(battler: any): void; // TODO
+    update(): void;
+    animationWait(): number;
+    updateIcon(): void;
+    updateFrame(): void;
+}
+
+declare class Sprite_StateOverlay extends Sprite_Base
+{
+    initialize(): void;
+    initMembers(): void;
+    loadBitmap(): void;
+    setup(battler: any): void; // TODO
+    update(): void;
+    animationWait(): number;
+    updatePattern(): void;
+    updateFrame(): void;
+}
+
+declare class Sprite_Weapon extends Sprite_Base
+{
+    initialize(): void;
+    initMembers(): void;
+    setup(weaponImageId: number): void;
+    update(): void;
+    animationWait(): number;
+    updatePattern(): void;
+    loadBitmap(): void;
+    updateFrame(): void;
+    isPlaying(): boolean;
+}
+
+declare class Sprite_Balloon extends Sprite_Base
+{
+    initialize(): void;
+    initMembers(): void;
+    loadBitmap(): void;
+    setup(balloonId: number): void;
+    update(): void;
+    updateFrame(): void;
+    speed(): number;
+    waitTime(): number;
+    frameIndex(): number;
+    isPlaying(): boolean;
+}
+
+declare class Sprite_Picture extends Sprite
+{
+    initialize(): void;
+    initialize(pictureId: number): void;
+    picture(): any; // TODO
+    update(): void;
+    updateBitmap(): void;
+    updateOrigin(): void;
+    updatePosition(): void;
+    updateScale(): void;
+    updateTone(): void;
+    updateOther(): void;
+    loadBitmap(): void;
+}
+
+declare class Sprite_Timer extends Sprite
+{
+    initialize(): void;
+    createBitmap(): void;
+    update(): void;
+    updateBitmap(): void;
+    redraw(): void;
+    timerText(): string;
+    updatePosition(): void;
+    updateVisibility(): void;
+}
+
+declare class Sprite_Destination extends Sprite
+{
+    initialize(): void;
+    update(): void;
+    createBitmap(): void;
+    updatePosition(): void;
+    updateAnimation(): void;
+}
+
+declare class Spriteset_Base extends Sprite
+{
+    initialize(): void;
+    createLowerLayer(): void;
+    createUpperLayer(): void;
+    update(): void;
+    createBaseSprite(): void;
+    createToneChanger(): void;
+    createCanvasToneChanger(): void;
+    createPictures(): void;
+    createTimer(): void;
+    createScreenSprites(): void;
+    updateScreenSprites(): void;
+    updateToneChanger(): void;
+    updateWebGLToneChanger(): void;
+    updateCanvasToneChanger(): void;
+    updatePosition(): void;
+}
+
+declare class Spriteset_Map extends Spriteset_Base
+{
+    initialize(): void;
+    createLowerLayer(): void;
+    update(): void;
+    hideCharacters(): void;
+    createParallax(): void;
+    createTilemap(): void;
+    loadTileset(): void;
+    createCharacters(): void;
+    createShadow(): void;
+    createDestination(): void;
+    createWeather(): void;
+    updateTileset(): void;
+
+    _canvasReAddParallax(): void;
+
+    updateParallax(): void;
+    updateTilemap(): void;
+    updateShadow(): void;
+    updateWeather(): void;
+}
+
+declare class Spriteset_Battle extends Spriteset_Base
+{
+    initialize(): void;
+    createLowerLayer(): void;
+    createBackground(): void;
+    update(): void;
+    createBattleField(): void;
+    createBattleback(): void;
+    updateBattleback(): void;
+    locateBattleback(): void;
+    battleback1Bitmap(): Bitmap;
+    battleback2Bitmap(): Bitmap;
+    battleback1Name(): string;
+    battleback2Name(): string;
+    overworldBattleback1Name(): string;
+    overworldBattleback2Name(): string;
+    normalBattleback1Name(): string;
+    normalBattleback2Name(): string;
+    terrainBattleback1Name(): string;
+    terrainBattleback2Name(): string;
+    defaultBattleback1Name(): string;
+    defaultBattleback2Name(): string;
+    shipBattleback1Name(): string;
+    shipBattleback2Name(): string;
+    autotileType(z: number): any; // TODO
+    createEnemies(): void;
+    compareEnemySprite(a: any, b: any): number; // TODO
+    createActors(): void;
+    updateActors(): void;
+    battlerSprites(): any; // TODO
+    isAnimationPlaying(): boolean;
+    isEffecting(): boolean;
+    isAnyoneMoving(): boolean;
+    isBusy(): boolean;
+}
