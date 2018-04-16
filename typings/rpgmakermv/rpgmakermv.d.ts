917929b19f652b89c087829a8b356c5512f407b3
aoitaku <aoitaku@gmail.com>
2018-04-17 04:35:30 +0900

    feat: pixi.js の型定義を npm 経由で取得するように

--- typings/rpgmakermv/rpgmakermv.d.ts
+++ typings/rpgmakermv/rpgmakermv.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="../lib/lib.d.ts" />
-/// <reference path="../pixi.js/pixi-filters.d.ts" />
-/// <reference path="../pixi.js/pixi-tilemap.d.ts" />
+/// <reference types="pixi-tilemap" />
 
 declare class JsExtensions
 {
